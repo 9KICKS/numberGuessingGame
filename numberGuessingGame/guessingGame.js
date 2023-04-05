b54a4cde56cf6fae068a8539ab0c7512a9407eee
9KICKS <officialjoshua9@gmail.com>
2023-04-05 22:47:01 +0100

    "updated number guessing game"

--- numberGuessingGame/guessingGame.js
+++ numberGuessingGame/guessingGame.js
@@ -1,31 +1,55 @@
 function guessGame() {
+    const userGuessInput = document.getElementById("userGuessInput");
+    const guessButton = document.getElementById("guessButton");
+    const userGuessText = document.getElementById("userGuessText");
+    const playButton = document.getElementById("playButton");
+    const exitButton = document.getElementById("exitButton");
+
     let tries = 0;
     const maxTries = 5;
     let isCorrect = false;
-    const jackpotNumber = Math.floor(Math.random() * 1000) + 1;
+    let jackpotNumber = Math.floor(Math.random() * 1000) + 1;
 
-    while (tries < maxTries && !isCorrect) {
-        const userGuess = parseInt(prompt(`Guess a number between 1 and 1000. You have ${maxTries - tries} attempts remaining.`));
+    function resetGame() {
+        tries = 0;
+        isCorrect = false;
+        jackpotNumber = Math.floor(Math.random() * 1000) + 1;
+        userGuessText.innerText = "Guess a number between 1 and 1000";
+        userGuessInput.disabled = false;
+        guessButton.disabled = false;
+        playButton.disabled = true;
+        exitButton.disabled = false;
+    }
+
+    function exitGame() {
+        userGuessText.innerText = "Thanks for playing.";
+        userGuessInput.disabled = true;
+        guessButton.disabled = true;
+        playButton.disabled = false;
+        exitButton.disabled = true;
+    }
+
+    function guess() {
+        const userGuess = parseInt(userGuessInput.value);
         if (userGuess === jackpotNumber) {
-            alert("Congratulations! You won.");
+            userGuessText.innerText = "Congratulations! You won.";
             isCorrect = true;
+            exitGame();
+        } else if (tries >= maxTries - 1) {
+            userGuessText.innerText = `You lost! The correct number was ${jackpotNumber}.`;
+            exitGame();
+        } else if (userGuess < jackpotNumber) {
+            userGuessText.innerText = `You guessed ${userGuess}. Try a higher number. You have ${maxTries - tries - 1} attempts remaining.`;
         } else {
-            alert(`You guessed ${userGuess}. Try again!`);
+            userGuessText.innerText = `You guessed ${userGuess}. Try a lower number. You have ${maxTries - tries - 1} attempts remaining.`;
         }
         tries++;
+        userGuessInput.value = "";
     }
 
-    if (!isCorrect) {
-        let response = prompt(`You lost! Better luck next time. The correct number was ${jackpotNumber}. Would you like to reset or quit game? Enter "reset" or "quit".`);
-        while (response !== "reset" && response !== "quit") {
-            response = prompt(`Invalid response. Please enter "reset" or "quit".`);
-        }
-        if (response === "reset") {
-            guessGame();
-        } else {
-            alert("Thanks for playing.");
-        }
-    }
+    guessButton.addEventListener("click", guess);
+    playButton.addEventListener("click", resetGame);
+    exitButton.addEventListener("click", exitGame);
 }
 
 guessGame();
